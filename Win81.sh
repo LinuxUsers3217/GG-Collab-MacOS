4bc4169ea417e77c429957d5e0fd23c0cc12834b
T-Rex21996.1 <88968137+TRex219961@users.noreply.github.com>
2022-06-06 09:07:27 +0700

    Update Win81.sh

--- Win81.sh
+++ Win81.sh
@@ -1,3 +1,8 @@
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip -o ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+rm -rf ngrok-stable-linux-amd64.zip
+./ngrok authtoken 268tKkixRMBBxHKeBCpS6s72ohm_7UvxkT4VkysGcyKi2JpZN
+nohup ./ngrok tcp --region ap 5900 &>/dev/null &
 sudo apt-get update && apt-get install qemu -y
 sudo apt install qemu-utils -y
 sudo apt install qemu-system-x86-xen -y
