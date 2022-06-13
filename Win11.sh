592d3c214475cb249520666cad53fa4604c26592
T-Rex21996.1 <88968137+TRex219961@users.noreply.github.com>
2022-06-13 14:51:44 +0700

    Update Win11.sh

--- Win11.sh
+++ Win11.sh
@@ -9,7 +9,7 @@ sudo apt install qemu-system-x86-xen -y
 sudo apt install qemu-system-x86 -y
 qemu-img create -f raw windows10.img 60G
 wget -O virtio-win.iso 'https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.215-1/virtio-win-0.1.215.iso'
-wget -O windows11.iso 'https://bom.so/Snyskz'
+wget -O windows11.iso 'https://bom.so/i1bBeT'
 clear
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 sudo qemu-system-x86_64 \
