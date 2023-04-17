d7d27c866e4e8351eab7fdaca437b714d1db47fe
T-Rex21996.1 <88968137+TRex219961@users.noreply.github.com>
2023-04-17 20:48:06 +0700

    Update xxx.sh

--- xxx.sh
+++ xxx.sh
@@ -18,7 +18,6 @@ curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*publ
 sudo qemu-system-x86_64 \
   -m 4G \
   -cpu EPYC \
-  -enable-kvm \
   -boot order=d \
   -drive file=Tiny10.iso,media=cdrom \
   -drive file=Tiny10.img,format=raw \
